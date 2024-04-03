c593f94e0d888156b81a1b6f3c0e83d5a7f37a71
William IEHL <www.iehl@gmail.com>
2024-04-03 10:10:54 +0700

    Adjust cmp tab action to preference

--- lua/fanzy/lazy/snippets.lua
+++ lua/fanzy/lazy/snippets.lua
@@ -1,9 +1,3 @@
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 return {
   {
     "L3MON4D3/LuaSnip",
@@ -62,14 +56,14 @@ return {
           --["<C-Space>"] = cmp.mapping.complete(),
           --["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
+            local matching = vim.tbl_filter(function(item) if item.exact then return true end end, cmp.get_entries())
+
+            if cmp.visible() and cmp.get_selected_entry() ~= nil then
+              cmp.confirm()
+            elseif cmp.visible() and not vim.tbl_isempty(matching) then
               cmp.confirm({ select = true })
-              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-              -- that way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
+            elseif luasnip.jumpable(1) then
+              luasnip.jump(1)
             else
               fallback()
             end
